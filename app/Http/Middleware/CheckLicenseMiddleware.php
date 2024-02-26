86ad781fce931e3500df760390558a18539c4c29
Munti_Holan <muntiholan88@gmail.com>
2024-02-26 14:31:44 +0700

    update funsi lock

--- app/Http/Middleware/CheckLicenseMiddleware.php
+++ app/Http/Middleware/CheckLicenseMiddleware.php
@@ -46,7 +46,7 @@ class CheckLicenseMiddleware
         $html = "<div align='center'>
             <table width='100%' border='0' style='padding:15px; border-color:#F00; border-style:solid; background-color:#FF6C70; font-family:Tahoma, Geneva, sans-serif; font-size:22px; color:white;'>
                 <tr>
-                    <td><b>You don't have permission to use this product. The message from server is: <%returnmessage%> <br> Contact the product developer.</b></td>
+                    <td><b>Please contact your developer for update aplication.!!!<br> <%returnmessage%> </b></td>
                 </tr>
             </table>
         </div>";
